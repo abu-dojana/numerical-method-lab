3717712f003fe237c83edebd4d2d0711487f9cdf
Abu Dojana Tahmid <abudojana2001@gmail.com>
2023-11-07 09:53:52 +0600

    Create false-position_lab1.cpp
    Lab -1 
    see class lecture

---
+++ false-position_lab1.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <stdlib.h>
+using namespace std;
+
+float f(float x)
+{
+    return x*x*x - x -1;
+}
+
+void bisection()
+{
+    float x0, x1 , x2 , e;
+
+    cout << "Enter x0:";
+    cin >> x0;
+
+    cout << "Enter x1:";
+    cin >> x1;
+
+    cout << "Enter e:";
+    cin >> e;
+
+    if (f(x0)* f(x1)>0)
+    {
+        cout << "WRONG GUESS!" << endl;
+    }
+    else
+    {
+        do
+        {
+            x2 = x0 - ( f(x0) * ( (x0 - x1)/ (f(x0) - f(x1) ))) ;
+
+            if ( f(x0)*f(x2) <0)
+            {
+                x1=x2;
+            }
+            else if ( f(x0)*f(x2) >0 )
+            {
+                x0 = x2;
+            }
+            cout << "root: " << x2 << ", " << "f(x)= " << f(x2) << endl;
+        }while(fabs(f(x2))>e); // e = -0.001 or 0.001 doesn't matter. fabs does that.
+            cout << "Final Root is: " << x2 << endl;
+    }
+
+}
+
+int main()
+{
+    bisection();
+    return 0;
+}
